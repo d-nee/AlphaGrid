f88007cb9ab245c393897c1b32754ff098426646
Daniel Nee <d-nee@github.com>
2021-06-07 11:32:30 -0700

    added models

--- GridPit.py
+++ GridPit.py
@@ -12,7 +12,7 @@ use this script to play any two agents against each other, or play manually with
 any agent.
 """
 
-human_vs_cpu = False
+p2_is_not_nn = True
 
 
 g = GridGame(5)
@@ -35,21 +35,21 @@ def randomplay(b):
 
 # nnet players
 n1 = NNet(g)
-n1.load_checkpoint('./temp_old_2_symmetry/','best.pth.tar')
-args1 = dotdict({'numMCTSSims': 50, 'cpuct':1.0})
+n1.load_checkpoint('models_2s_4d','best.pth.tar')
+args1 = dotdict({'numMCTSSims': 100, 'cpuct':1.0})
 mcts1 = MCTS(g, n1, args1)
 n1p = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
 
 with open("pitlog.txt", "w") as r:
     print("cleared log")
 
-if human_vs_cpu:
+if p2_is_not_nn:
     # player2 = humanplay
     player2 = randomplay
 else:
     n2 = NNet(g)
     n2.load_checkpoint('./temp_old_8_symmetry/', 'best.pth.tar')
-    args2 = dotdict({'numMCTSSims': 50, 'cpuct': 1.0})
+    args2 = dotdict({'numMCTSSims': 100, 'cpuct': 1.0})
     mcts2 = MCTS(g, n2, args2)
     n2p = lambda x: np.argmax(mcts2.getActionProb(x, temp=0))
     player2 = n2p  # Player 2 is neural network if it's cpu vs cpu.

3e4012367d99fd7da1765574242fd7d905d18f15
Devin Hartzell <devin@caltech.edu>
2021-06-07 21:23:32 -0700

    add more gifs

--- GridPit.py
+++ GridPit.py
@@ -13,7 +13,7 @@ use this script to play any two agents against each other, or play manually with
 any agent.
 """
 
-p2_is_not_nn = True
+p2_is_not_nn = False
 
 
 g = GridGame(5)
@@ -36,7 +36,7 @@ def randomplay(b):
 
 # nnet players
 n1 = NNet(g)
-n1.load_checkpoint('models_2s_4d','best.pth.tar')
+n1.load_checkpoint('models_2s_4d','checkpoint_3.pth.tar')
 args1 = dotdict({'numMCTSSims': 50, 'cpuct':1.0})
 mcts1 = MCTS(g, n1, args1)
 n1p = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
@@ -49,7 +49,7 @@ if p2_is_not_nn:
     player2 = randomplay
 else:
     n2 = NNet(g)
-    n2.load_checkpoint('models_2s_4d','best.pth.tar')
+    n2.load_checkpoint('models_2s_4d','checkpoint_8.pth.tar')
     args2 = dotdict({'numMCTSSims': 100, 'cpuct': 1.0})
     mcts2 = MCTS(g, n2, args2)
     n2p = lambda x: np.argmax(mcts2.getActionProb(x, temp=0))
@@ -57,4 +57,4 @@ else:
 
 arena = Arena.Arena(n1p, player2, g, display=GridGame.display)
 
-print(arena.playGames(40, verbose=False, visualizer=SaveToFileVisualizer, viz_args={'path': 'gameviz'}))
+print(arena.playGames(40, verbose=False, visualizer=SaveToFileVisualizer, viz_args={'path': 'gameviz/4v8'}))
